3a05ecb09757f6d660bcf6e3b7d67fcec938d402
Denys Koniakhin <denyskoniakhin@Denyss-MacBook-Air.local>
2026-07-06 21:56:20 -0600

    Fix lightbox image overflow on desktop

--- components/ImageLightbox.tsx
+++ components/ImageLightbox.tsx
@@ -22,14 +22,14 @@ export default function ImageLightbox({
       className="no-print fixed inset-0 z-50 bg-black/80 flex flex-col items-center justify-center p-4"
       onClick={onClose}
     >
-      <div className="flex gap-2 max-w-full overflow-x-auto">
+      <div className="flex flex-col sm:flex-row gap-2 items-center justify-center w-full max-w-5xl">
         {images.map((img) => (
           // eslint-disable-next-line @next/next/no-img-element
           <img
             key={img}
             src={exerciseImageUrl(img)}
             alt={title}
-            className="max-h-[70vh] rounded-lg"
+            className="rounded-lg object-contain min-w-0 max-h-[38vh] sm:max-h-[70vh] max-w-[90vw] sm:max-w-[calc(50%-0.25rem)]"
           />
         ))}
       </div>
